1519ad652a3c82078bad82ae3dc4ee93281a704a
Radia Johnson <johnson.radia@gmail.com>
2016-11-24 11:06:53 -0500

    Update starINDEXmus.sh

--- mus/starINDEXmus.sh
+++ mus/starINDEXmus.sh
@@ -5,8 +5,8 @@ module add STAR
 module add SAMtools
 
 # Go to the STAR directory in the PARK_LAB folder
-#cd /RQexec/johnsonr/Mus_musculus/UCSC/mm10/Sequence/Chromosomes/
+cd /RQexec/johnsonr/Mus_musculus/UCSC/mm10/Sequence/Chromosomes/
 
 # Build a STAR genome index into the Chromosomes folder
-#STAR  --runMode genomeGenerate --genomeDir ./ --genomeFastaFiles mm10.fa --runThreadN 4 --sjdbGTFfile /RQexec/johnsonr/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.gtf  --sjdbOverhang 99
+STAR  --runMode genomeGenerate --genomeDir ./ --genomeFastaFiles mm10.fa --runThreadN 4 --sjdbGTFfile /RQexec/johnsonr/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.gtf  --sjdbOverhang 99
 
