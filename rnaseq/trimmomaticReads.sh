5db996750a02b29163047a7d57338007dc392e0e
Radia Johnson <johnson.radia@gmail.com>
2016-11-24 13:04:15 -0500

    Create trimmomaticReads.sh

---
+++ rnaseq/trimmomaticReads.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+FILENAME=$1
+count=0
+
+while read pre
+        do
+                let count++
+                echo "$count $pre"
+
+java -jar $HOME/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 -trimlog logs/$pre.log ${pre}_R1.f*q.gz ${pre}_R2.f*q.gz ${pre}_1.trim.fq.gz ${pre}_1.unpaired.fq.gz ${pre}_2.trim.fq.gz ${pre}_2.unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10:8:true LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+done < $FILENAME
+
+echo -e "\nTotal $count lines read"
+
+
+
+
+
