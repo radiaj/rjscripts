f44dfa218eaf1360662a56916661c367ccd3b7ba
Radia Johnson <johnson.radia@gmail.com>
2016-12-20 15:35:31 -0500

    Create getSortedBamnIndex.sh

---
+++ rnaseq/getSortedBamnIndex.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Add the modules needed for the analysis
+module add SAMtools
+# Make sure samtools is installed on linux
+
+# Variables
+FILENAME=$1
+# If $2 is not passed, set the working directory to current directory
+myFILEDIR="${2:-${PWD}}"
+count=0
+
+while read mySIZE
+        do
+                let count++
+                echo "$count $mySIZE"
+                echo "Directory set at $myFILEDIR"
+                # Go to the STAR directory in the PARK_LAB folder
+                cd "${myFILEDIR}"/"${mySIZE}"_STAR/
+
+                # Convert the sam file directly to a sorted bam file
+                samtools sort "${mySIZE}"*.bam "${mySIZE}"_sorted
+                samtools index "${mySIZE}"_sorted.bam "${mySIZE}"_sorted.bai &
+
+done < $FILENAME
+
+echo -e "\nTotal $count lines read"
+
+wait
+
