6789c350c1f5b789a48d56216f070c20c50f3117
Radia Johnson <johnson.radia@gmail.com>
2017-08-09 11:57:04 -0400

    Create runxenomes.sh

---
+++ pdx/runxenomes.sh
@@ -0,0 +1,20 @@
+#--------------------------
+# To run xenomes
+#--------------------------
+#!/bin/bash
+
+
+cd /media/cyberlogic/a384d9c7-6dda-4d1b-b32d-9a6b0e007931/toto/PARK_LAB_DATA_FILES/PDX_RNAseq_Files/
+# xenome index -T 8 -P idx -H mm10.fa -G hg19.fa
+
+while read mySIZE myFASTQ1 myFASTQ2
+        do
+                let count++
+                echo "$count $mySIZE"
+                echo "fastq1 $myFASTQ1"
+
+                xenome classify -T 8 -P idx --pairs --host-name mm10 --graft-name hg19 -i ./"${myFASTQ1}" -i ./"${myFASTQ2}"
+
+done < $FILENAME
+
+echo -e "\nTotal $count lines read"
